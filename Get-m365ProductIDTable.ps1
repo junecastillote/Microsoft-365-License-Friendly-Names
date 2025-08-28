7c9e8fb040ff13294c274809973f3f9a4dfe7531
June Castillote <june.castillote@gmail.com>
2025-08-28 09:07:09 +0800

    Update Get-m365ProductIDTable.ps1

--- Get-m365ProductIDTable.ps1
+++ Get-m365ProductIDTable.ps1
@@ -1,6 +1,6 @@
 <#PSScriptInfo
 
-.VERSION 1.3
+.VERSION 1.4
 
 .GUID 79801e88-d136-4955-8730-07ae1dd65cb1
 
@@ -10,7 +10,7 @@
 
 .COPYRIGHT june.castillote@gmail.com
 
-.TAGS Office365 License Friendly Microsoft365
+.TAGS Office 365 License, License Name, Friendly Name, Microsoft 365 License Name
 
 .LICENSEURI https://github.com/junecastillote/Microsoft-365-License-Friendly-Names/blob/master/LICENSE
 
@@ -32,7 +32,7 @@
 .DESCRIPTION
  Get license IDs and friendly names directly from MS article in GitHub
 .SYNOPSIS
-    Get the friendly names from the Microft Document "Product names and service plan identifiers for licensing"
+    Get the friendly names from the Microsoft Document "Product names and service plan identifiers for licensing"
 .DESCRIPTION
     This script downloads and parses the licensing-service-plan-reference.md file from GitHub and converts to a PowerShell object.
 .EXAMPLE
@@ -42,17 +42,17 @@
     PS C:\> .\Get-m365ProductIDTable.ps1 | Export-Csv -NoTypeInformation .\m365-License-Reference.csv
     Get the product names and service plan identifiers and export to CSV.
 .EXAMPLE
-    PS C:\> .\Get-m365ProductIDTable.ps1 -TitleCase
-    Get the product names and service plan identifiers online and display the result on the screen. The friendly names will be convered to title case.
-.EXAMPLE
     PS C:\> .\Get-m365ProductIDTable.ps1 -SkuId 245e6bf9-411e-481e-8611-5c08595e2988
     Get the product names and service plan identifiers that matches the specified SkuId
+.EXAMPLE
+    PS C:\> .\Get-m365ProductIDTable.ps1 -SkuPartNumber SPE_E5
+    Get the product names and service plan identifiers that matches the specified SkuPartNumber
 .EXAMPLE
     PS C:\> .\Get-m365ProductIDTable.ps1 -ForceOnline
     Force to download the SKU table from the online source and ignoring the locally available table version.
 #>
 
-[CmdletBinding()]
+[CmdletBinding(DefaultParameterSetName = 'Default')]
 param (
     ## This is URL path to the the licensing reference table document from GitHub.
     ## The current working URL is the default value.
@@ -62,10 +62,16 @@ param (
     $URL = 'https://raw.githubusercontent.com/MicrosoftDocs/entra-docs/main/docs/identity/users/licensing-service-plan-reference.md',
 
     # Return only the matching SkuId
-    [Parameter()]
+    [Parameter(ParameterSetName = 'SkuId', Mandatory)]
     [ValidateNotNullOrEmpty()]
     [guid]
     $SkuId,
+
+    # Return only the matching SkuPartNumber
+    [Parameter(ParameterSetName = 'SkuPartNumber', Mandatory)]
+    [ValidateNotNullOrEmpty()]
+    [string]
+    $SkuPartNumber,
 
     ## Force convert license names to title case.
     [parameter()]
@@ -75,26 +81,44 @@ param (
     ## Force to download the online version instead of checking table in the current session
     [parameter()]
     [switch]
-    $ForceOnline
+    $ForceOnline,
+
+    ## Specifiy the list delimiter for ChildServicePlan and ChildServicePlanName.
+    ## Default character delimited is comma ","
+    [parameter()]
+    [string]
+    $ListDelimiterCharacter
 )
 
-Function ShowResult {
-    if ($SkuId) {
-        $Global:SkuTable | Where-Object { $_.SkuId -eq $SkuId }
-    }
-    else {
-        $Global:SkuTable
+function ShowResult {
+    $visible_properties = [string[]]@('SkuName', 'SkuPartNumber', 'SkuId')
+    [Management.Automation.PSMemberInfo[]]$default_properties = [System.Management.Automation.PSPropertySet]::new('DefaultDisplayPropertySet', $visible_properties )
+    $Global:SkuTable | Add-Member -MemberType MemberSet -Name PSStandardMembers -Value $default_properties -Force
+
+    switch ($PSCmdlet.ParameterSetName) {
+        SkuId {
+            Write-Verbose "Filtering by SkuId"
+            $Global:SkuTable | Where-Object { $_.SkuId -eq $SkuId }
+        }
+        SkuPartNumber {
+            Write-Verbose "Filtering by SkuPartNumber"
+            $Global:SkuTable | Where-Object { $_.SkuPartNumber -eq $SkuPartNumber }
+        }
+        default {
+            Write-Verbose "No filtering. Showing all results."
+            $Global:SkuTable
+        }
     }
 }
 
 $ErrorActionPreference = 'STOP'
 
-if ($ForceOnline) { $global:SkuTable = @() }
+if ($ForceOnline) { $Global:SkuTable = @() }
 
 #https://learn.microsoft.com/en-us/entra/identity/users/licensing-service-plan-reference
 
 # Check first if the SKU table is already available in the session. This ensures that the script only downloads the online table once per session, unless the -ForceOnline switch is used.
-if ($global:SkuTable) {
+if ($Global:SkuTable) {
     Write-Verbose "SKU table exists in session."
     return ShowResult
 }
@@ -103,7 +127,8 @@ else {
 
     ## Parse the Markdown Table from the $URL
     try {
-        [System.Collections.ArrayList]$raw_Table = ([System.Net.WebClient]::new()).DownloadString($URL).split("`n")
+        $raw_Table = Invoke-RestMethod -Uri $URL -ErrorAction Stop
+        $raw_Table = $raw_Table -split "`n"
     }
     catch {
         Write-Output "There was an error getting the licensing reference table at [$URL]. Please make sure that the URL is still valid."
@@ -125,11 +150,11 @@ else {
     }
 
     ## Perform a little clean-up
-    ### replace "[space] | [space]" with "|"
-    ### replace "[space]<br/>[space]" with ","
-    ### replace "((" with "("
-    ### replace "))" with ")"
-    ### #replace ")[space](" with ")("
+    ## replace "[space] | [space]" with "|"
+    ## replace "[space]<br/>[space]" with ","
+    ## replace "((" with "("
+    ## replace "))" with ")"
+    ## #replace ")[space](" with ")("
 
     $result = $result `
         -replace '\s*\|\s*', '|' `
@@ -138,19 +163,37 @@ else {
         -replace '\)\)', ')' `
         -replace '\)\s*\(', ')('
 
+    # Force title case conversion if -TitleCase is not used.
+    if (-not $PSBoundParameters.ContainsKey('TitleCase')) {
+        $TitleCase = $true
+        Write-Verbose "TitleCase name conversion enabled"
+    }
+
     ## Create the result object
-    if ($TitleCase) {
-        $TextInfo = (Get-Culture).TextInfo
-        $Global:SkuTable = @(
-            $result | ConvertFrom-Csv -Delimiter "|" -Header 'SkuName', 'SkuPartNumber', 'SkuID', 'ChildServicePlan', 'ChildServicePlanName' |
-            Select-Object @{n = 'SkuName' ; e = { $TextInfo.ToTitleCase($_.SkuName) } }, 'SkuPartNumber', 'SkuID', 'ChildServicePlan', @{n = 'ChildServicePlanName' ; e = { $TextInfo.ToTitleCase($_.ChildServicePlanName) } }
-        )
-    }
-    else {
-        $Global:SkuTable = @(
-            $result | ConvertFrom-Csv -Delimiter "|" -Header 'SkuName', 'SkuPartNumber', 'SkuID', 'ChildServicePlan', 'ChildServicePlanName'
-        )
-    }
+    $TextInfo = (Get-Culture).TextInfo
+
+    # Set "," (comma) as the default delimiter character for ChildServicePlan and ChildServicePlanName
+    if (-not $ListDelimiterCharacter) { $ListDelimiterCharacter = "," }
+    $Global:SkuTable = @($result | ConvertFrom-Csv -Delimiter "|" -Header 'SkuName', 'SkuPartNumber', 'SkuID', 'ChildServicePlan', 'ChildServicePlanName' | ForEach-Object {
+
+
+            if ($ListDelimiterCharacter -ne ",") {
+                $childServicePlan = (([string]$_.ChildServicePlan).Split(",") | Sort-Object) -join $ListDelimiterCharacter
+                $childServicePlanName = (([string]$_.ChildServicePlanName).Split(",") | Sort-Object) -join $ListDelimiterCharacter
+            }
+            else {
+                $childServicePlan = $_.ChildServicePlan
+                $childServicePlanName = $_.ChildServicePlanName
+            }
+
+            [pscustomobject]@{
+                SkuName              = if ($TitleCase) { $TextInfo.ToTitleCase($_.SkuName) } else { $_.SkuName }
+                SkuPartNumber        = $_.SkuPartNumber
+                SkuId                = [guid]$_.SkuId
+                ChildServicePlan     = $childServicePlan
+                ChildServicePlanName = if ($TitleCase) { $TextInfo.ToTitleCase($childServicePlanName) } else { $childServicePlanName }
+            }
+        })
 
     ## return the result
     return ShowResult
