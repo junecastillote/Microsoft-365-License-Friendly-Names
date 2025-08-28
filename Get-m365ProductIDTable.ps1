5cab36408879aa02c04c6578607c1f48a27dc5d2
June Castillote <june.castillote@gmail.com>
2025-08-28 09:17:53 +0800

    Update Get-m365ProductIDTable.ps1

--- Get-m365ProductIDTable.ps1
+++ Get-m365ProductIDTable.ps1
@@ -96,10 +96,12 @@ function ShowResult {
     $Global:SkuTable | Add-Member -MemberType MemberSet -Name PSStandardMembers -Value $default_properties -Force
 
     switch ($PSCmdlet.ParameterSetName) {
+        # -SkuId <GUID>
         SkuId {
             Write-Verbose "Filtering by SkuId"
             $Global:SkuTable | Where-Object { $_.SkuId -eq $SkuId }
         }
+        # -SkuPartNumber <SKU PART NUMBER>
         SkuPartNumber {
             Write-Verbose "Filtering by SkuPartNumber"
             $Global:SkuTable | Where-Object { $_.SkuPartNumber -eq $SkuPartNumber }
@@ -122,79 +124,79 @@ if ($Global:SkuTable) {
     Write-Verbose "SKU table exists in session."
     return ShowResult
 }
-else {
-    Write-Verbose "Downloading SKU table online..."
 
-    ## Parse the Markdown Table from the $URL
-    try {
-        $raw_Table = Invoke-RestMethod -Uri $URL -ErrorAction Stop
-        $raw_Table = $raw_Table -split "`n"
-    }
-    catch {
-        Write-Output "There was an error getting the licensing reference table at [$URL]. Please make sure that the URL is still valid."
-        Write-Output $_.Exception.Message
-        return $null
-    }
+# Continue if the SKU table is not yet in the session
+Write-Verbose "Downloading SKU table online..."
 
-    ## Determine the starting row index of the table
-    $startLine = ($raw_Table.IndexOf('| Product name | String ID | GUID | Service plans included | Service plans included (friendly names) |') + 1)
-
-    ## Determine the ending index of the table
-    $endLine = ($raw_Table.IndexOf('## Service plans that cannot be assigned at the same time') - 1)
-
-    ## Extract the string in between the lines $startLine and $endLine
-    $result = for ($i = $startLine; $i -lt $endLine; $i++) {
-        if ($raw_Table[$i] -notlike "*---*") {
-            $raw_Table[$i].Substring(1, $raw_Table[$i].Length - 1)
-        }
-    }
-
-    ## Perform a little clean-up
-    ## replace "[space] | [space]" with "|"
-    ## replace "[space]<br/>[space]" with ","
-    ## replace "((" with "("
-    ## replace "))" with ")"
-    ## #replace ")[space](" with ")("
-
-    $result = $result `
-        -replace '\s*\|\s*', '|' `
-        -replace '\s*<br/>\s*', ',' `
-        -replace '\(\(', '(' `
-        -replace '\)\)', ')' `
-        -replace '\)\s*\(', ')('
-
-    # Force title case conversion if -TitleCase is not used.
-    if (-not $PSBoundParameters.ContainsKey('TitleCase')) {
-        $TitleCase = $true
-        Write-Verbose "TitleCase name conversion enabled"
-    }
-
-    ## Create the result object
-    $TextInfo = (Get-Culture).TextInfo
-
-    # Set "," (comma) as the default delimiter character for ChildServicePlan and ChildServicePlanName
-    if (-not $ListDelimiterCharacter) { $ListDelimiterCharacter = "," }
-    $Global:SkuTable = @($result | ConvertFrom-Csv -Delimiter "|" -Header 'SkuName', 'SkuPartNumber', 'SkuID', 'ChildServicePlan', 'ChildServicePlanName' | ForEach-Object {
-
-
-            if ($ListDelimiterCharacter -ne ",") {
-                $childServicePlan = (([string]$_.ChildServicePlan).Split(",") | Sort-Object) -join $ListDelimiterCharacter
-                $childServicePlanName = (([string]$_.ChildServicePlanName).Split(",") | Sort-Object) -join $ListDelimiterCharacter
-            }
-            else {
-                $childServicePlan = $_.ChildServicePlan
-                $childServicePlanName = $_.ChildServicePlanName
-            }
-
-            [pscustomobject]@{
-                SkuName              = if ($TitleCase) { $TextInfo.ToTitleCase($_.SkuName) } else { $_.SkuName }
-                SkuPartNumber        = $_.SkuPartNumber
-                SkuId                = [guid]$_.SkuId
-                ChildServicePlan     = $childServicePlan
-                ChildServicePlanName = if ($TitleCase) { $TextInfo.ToTitleCase($childServicePlanName) } else { $childServicePlanName }
-            }
-        })
-
-    ## return the result
-    return ShowResult
+## Parse the Markdown Table from the $URL
+try {
+    $raw_Table = Invoke-RestMethod -Uri $URL -ErrorAction Stop
+    $raw_Table = $raw_Table -split "`n"
 }
+catch {
+    Write-Output "There was an error getting the licensing reference table at [$URL]. Please make sure that the URL is still valid."
+    Write-Output $_.Exception.Message
+    return $null
+}
+
+## Determine the starting row index of the table
+$startLine = ($raw_Table.IndexOf('| Product name | String ID | GUID | Service plans included | Service plans included (friendly names) |') + 1)
+
+## Determine the ending index of the table
+$endLine = ($raw_Table.IndexOf('## Service plans that cannot be assigned at the same time') - 1)
+
+## Extract the string in between the lines $startLine and $endLine
+$result = for ($i = $startLine; $i -lt $endLine; $i++) {
+    if ($raw_Table[$i] -notlike "*---*") {
+        $raw_Table[$i].Substring(1, $raw_Table[$i].Length - 1)
+    }
+}
+
+## Perform a little clean-up
+## replace "[space] | [space]" with "|"
+## replace "[space]<br/>[space]" with ","
+## replace "((" with "("
+## replace "))" with ")"
+## #replace ")[space](" with ")("
+
+$result = $result `
+    -replace '\s*\|\s*', '|' `
+    -replace '\s*<br/>\s*', ',' `
+    -replace '\(\(', '(' `
+    -replace '\)\)', ')' `
+    -replace '\)\s*\(', ')('
+
+# Force title case conversion if -TitleCase is not used.
+if (-not $PSBoundParameters.ContainsKey('TitleCase')) {
+    $TitleCase = $true
+    Write-Verbose "TitleCase name conversion enabled"
+}
+
+## Create the result object
+$TextInfo = (Get-Culture).TextInfo
+
+# Set "," (comma) as the default delimiter character for ChildServicePlan and ChildServicePlanName
+if (-not $ListDelimiterCharacter) { $ListDelimiterCharacter = "," }
+$Global:SkuTable = @($result | ConvertFrom-Csv -Delimiter "|" -Header 'SkuName', 'SkuPartNumber', 'SkuID', 'ChildServicePlan', 'ChildServicePlanName' | ForEach-Object {
+
+
+        if ($ListDelimiterCharacter -ne ",") {
+            $childServicePlan = (([string]$_.ChildServicePlan).Split(",") | Sort-Object) -join $ListDelimiterCharacter
+            $childServicePlanName = (([string]$_.ChildServicePlanName).Split(",") | Sort-Object) -join $ListDelimiterCharacter
+        }
+        else {
+            $childServicePlan = $_.ChildServicePlan
+            $childServicePlanName = $_.ChildServicePlanName
+        }
+
+        [pscustomobject]@{
+            SkuName              = if ($TitleCase) { $TextInfo.ToTitleCase($_.SkuName) } else { $_.SkuName }
+            SkuPartNumber        = $_.SkuPartNumber
+            SkuId                = [guid]$_.SkuId
+            ChildServicePlan     = $childServicePlan
+            ChildServicePlanName = if ($TitleCase) { $TextInfo.ToTitleCase($childServicePlanName) } else { $childServicePlanName }
+        }
+    })
+
+## return the result
+return ShowResult
